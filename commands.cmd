95ac75f4a3394fc23c403f32799f4c3f17ddb113
Gordon Beeming <gordon@devstarops.com>
2022-07-24 18:05:11 +0200

    blog to redirect locally to blog path

--- commands.cmd
+++ commands.cmd
@@ -2,6 +2,6 @@ podman build -f .\Containerfile -t profile
 
 podman stop -a
 
-podman exec -it a708722e43bda38ea0739468fc80b6356e7e5ad0ac8dfde39eea2340c0c639af /usr/sbin/nginx -s reload
+podman exec -it d2c0efc761b4acd0b7fd3bc44a6c07c12c74d9a16c5655a6c5aa4f86f5859cb9 /usr/sbin/nginx -s reload
 
-podman run -p 8080:80 --name profile --restart unless-stopped --replace --tls-verify -d -v nginx.conf:/etc/nginx/nginx.conf -v ./resources/access.log:/var/log/nginx/access.log -v ./resources/error.log:/var/log/nginx/error.log -v ./resources/:/var/profile/ localhost/profile:latest
+podman run -p 8080:80 --name profile --restart unless-stopped --replace --tls-verify -d -v nginx.conf:/etc/nginx/nginx.conf -v ./resources/access.log:/var/log/nginx/access.log -v ./resources/error.log:/var/log/nginx/error.log -v ./resources/:/var/profile/ -v ./src/:/www/profile/ localhost/profile:latest
