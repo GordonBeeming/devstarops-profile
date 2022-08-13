12e390277f7472f1320f03288fc1a10f5d719e5a
Gordon Beeming <gordon@devstarops.com>
2022-08-13 18:22:27 +0200

    fixed book images on mobile phones #1

--- commands.cmd
+++ commands.cmd
@@ -4,4 +4,4 @@ podman stop -a
 
 podman exec -it d2c0efc761b4acd0b7fd3bc44a6c07c12c74d9a16c5655a6c5aa4f86f5859cb9 /usr/sbin/nginx -s reload
 
-podman run -p 8080:80 --name profile --restart unless-stopped --replace --tls-verify -d -v nginx.conf:/etc/nginx/nginx.conf -v ./resources/access.log:/var/log/nginx/access.log -v ./resources/error.log:/var/log/nginx/error.log -v ./resources/:/var/profile/ -v ./src/:/www/profile/ localhost/profile:latest
+podman run -p 8100:80 --name profile --restart unless-stopped --replace --tls-verify -d -v nginx.conf:/etc/nginx/nginx.conf -v ./resources/access.log:/var/log/nginx/access.log -v ./resources/error.log:/var/log/nginx/error.log -v ./resources/:/var/profile/ -v ./src/:/www/profile/ localhost/profile:latest
